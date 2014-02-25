37a4d22cccc6f0acbaa455673f0e1e048b68fcf5
Jason Dew <jason.dew@gmail.com>
2014-02-25 09:00:27 -0500

    Adding requires

--- lib/quickbooks-ruby.rb
+++ lib/quickbooks-ruby.rb
@@ -61,6 +61,8 @@ require 'quickbooks/model/purchase_order'
 require 'quickbooks/model/vendor_credit'
 require 'quickbooks/model/estimate'
 require 'quickbooks/model/invoice'
+require 'quickbooks/model/tax_rate'
+require 'quickbooks/model/tax_code'
 
 #== Services
 require 'quickbooks/service/service_crud'
@@ -84,6 +86,8 @@ require 'quickbooks/service/purchase'
 require 'quickbooks/service/purchase_order'
 require 'quickbooks/service/vendor_credit'
 require 'quickbooks/service/estimate'
+require 'quickbooks/service/tax_rate'
+require 'quickbooks/service/tax_code'
 
 module Quickbooks
   @@logger = nil
